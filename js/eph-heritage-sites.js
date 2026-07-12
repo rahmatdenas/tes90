12d5eb574bf000259ff6df777d6d362ec16f7eae
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-07-12 10:17:48 +0700

    Update eph-heritage-sites.js

--- js/eph-heritage-sites.js
+++ js/eph-heritage-sites.js
@@ -107,7 +107,7 @@ populateProvinceTypesData()
     if (loadingDesc && isFetching) {
        loadingDesc.innerHTML = `Data yang ditarik terlalu banyak. Harap menunggu, 3-5 menit lagi...`;
     }
-  }, 45000);
+  }, 30000);
        
        console.error("Data utama gagal dimuat. Cek koneksi atau server Wikidata.", error);
     }); 
